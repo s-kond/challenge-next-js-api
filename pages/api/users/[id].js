cdac969f5ef65d926fa2347856f893e0772d7edb
Stefan Kondring <kondring.s@gmx.de>
2022-10-14 17:29:08 +0200

    solution

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,3 +1,20 @@
+import {getUserById, deleteUser} from '../../../services/userService';
+
 export default async function handler(request, response) {
+  if (request.method === 'GET') {
+    const {id} = request.query;
+    console.log(id);
+    const data = await getUserById(id);
+    console.log(data);
+    if (data) {
+      return response.status(200).json(data);
+    } else {
+      return response.status(403).json({message: 'Error: No user found.'});
+    }
+  } else if (request.method === 'DELETE') {
+    const {id} = request.query;
+    const data = await deleteUser(id);
+    return response.status(200).json({message: 'User successfully deleted'});
+  }
   res.status(403).json({message: 'Error: request method not allowed.'});
 }
--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,6 +1,12 @@
+import {getAllUsers, createUser} from '../../../services/userService';
+
 export default async function handler(request, response) {
   if (request.method === 'GET') {
-    const data = [];
+    const data = await getAllUsers();
+    return response.status(200).json(data);
+  } else if (request.method === 'POST') {
+    const newUser = request.body;
+    const data = await createUser(newUser);
     return response.status(200).json(data);
   }
 
